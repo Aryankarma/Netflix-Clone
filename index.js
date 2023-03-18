eb3f1b72aa6544157a7f71e359940a62289ef9d1
Aryankarma <110737632+Aryankarma@users.noreply.github.com>
2023-03-18 19:22:57 +0530

    finishing touches 1

--- index.js
+++ index.js
@@ -49,9 +49,25 @@ document.querySelector(".learnJS").addEventListener("click", function(){
     document.querySelector(".learnJS").classList.add("focusOutlineBlue");
 });
 document.querySelector(".learnJS").addEventListener("blur", function(){
-    document.querySelector(".learnJS").classList.remove("focusOutlineBlue")
+    document.querySelector(".learnJS").classList.remove("focusOutlineBlue");
 });
 
+document.querySelector(".learnJS1").addEventListener("click", function(){
+    document.querySelector(".learnJS1").classList.add("focusOutlineBlue");
+});
+document.querySelector(".learnJS1").addEventListener("blur", function(){
+    document.querySelector(".learnJS1").classList.remove("focusOutlineBlue");
+});
+
+// document.querySelector(".learnJS4").addEventListener("click",setInterval(function(){
+//     document.querySelector(".learnJS4").onclick = function(){location.href='www.google.com'};
+// },1000));
+
+// document.querySelector(".learnJS3").addEventListener("click",function(){
+//     document.querySelector(".learnJS3").classList.add("buttonAnimation");   
+// });
+
+// document.querySelector(".learnJS4").onclick = "classList.add('buttonAnimation')";
 
 document.querySelector(".learnJS").addEventListener("change",abcd);
 
@@ -67,10 +83,35 @@ function abcd(){
         document.querySelector(".learnJS").classList.add("outlineGreen");
         document.querySelector(".learnJS").classList.remove("bottomLineYellow");
         document.querySelector(".learnJS2").classList.add("emailRequired");
+        document.querySelector(".learnJS").classList.remove("focusOutlineBlue");
     }else if(final != "@gmail.com"){
         document.querySelector(".learnJS").classList.add("bottomLineYellow");
         document.querySelector(".learnJS").classList.remove("outlineGreen");
         document.querySelector(".learnJS2").classList.remove("emailRequired");
+        document.querySelector(".learnJS").classList.remove("focusOutlineBlue");
+    }
+}
+
+document.querySelector(".learnJS1").addEventListener("change",abcde);
+
+function abcde(){
+    var x = document.querySelector(".learnJS1").value;
+    var lengthOfX = x.length;
+    var final = x.slice(lengthOfX - 10, lengthOfX);
+    if(x.length<=4){
+        document.querySelector(".learnJS3").innerHTML = "Email is required.";
+    }else if(x.length>4){
+        document.querySelector(".learnJS3").innerHTML = "Please enter a valid email address.";
+    }if(final === "@gmail.com"){
+        document.querySelector(".learnJS1").classList.add("outlineGreen");
+        document.querySelector(".learnJS1").classList.remove("bottomLineYellow");
+        document.querySelector(".learnJS3").classList.add("emailRequired");
+        document.querySelector(".learnJS1").classList.remove("focusOutlineBlue");
+    }else if(final != "@gmail.com"){
+        document.querySelector(".learnJS1").classList.add("bottomLineYellow");
+        document.querySelector(".learnJS1").classList.remove("outlineGreen");
+        document.querySelector(".learnJS3").classList.remove("emailRequired");
+        document.querySelector(".learnJS1").classList.remove("focusOutlineBlue");
     }
 }
 
@@ -93,4 +134,3 @@ document.querySelector(".pressEnter").addEventListener("click", function(){
 //         document.querySelector(".pressEnter").classList.add(".pressEnterCSS");
 //     },3000);
 // })
-
